987b10c74743aaa10aae3b6b9074a85546a71e3c
Gertobin11 <gertobin11@gmail.com>
2021-09-30 15:57:45 +0000

    added model and form to contact section

---
+++ contact/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import ContactForm
+
+
+class ContactForm1(forms.ModelForm):
+    class Meta:
+        model = ContactForm
+        fields = ('fullname', 'email', 'phone_number',
+                  'comments')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'fullname': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'comments': 'Comments',
+        }
+        for field in self.fields:
+            placeholder = placeholders[field]
+            self.fields['full_name'].widget.attrs['autofocus'] = True
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactForm(models.Model):
+    """ Model to store the contact information left by visitors to the site """
+    feedback_number = models.AutoField(primary_key=True)
+    fullname = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    comments = models.TextField(max_length=500, null=False, blank=True)
+
+    def __str__(self):
+        return self.fullname
