bd6295c21ef6fba15bc628c69d75d26152b3086b
Gertobin11 <gertobin11@gmail.com>
2021-10-04 11:32:59 +0000

    added fields to model and styles portfolio pge cards, fixed an issue with the scroll down button not disappearing at the bottom of the page , put all js in a post laod js block

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,9 +3,14 @@ from django.db import models
 
 class Portfolio(models.Model):
     project_name = models.CharField(max_length=254)
+    project_intro = models.CharField(max_length=254,
+                                     default='This is a project \
+                                     completed by Ger Tobin')
     project_description = models.TextField(max_length=1000)
     technologies_used = models.CharField(max_length=254)
     project_image = models.ImageField()
+    git_hub_repo = models.CharField(max_length=254,
+                                    default='https://github.com/Gertobin112')
     live_site = models.CharField(max_length=254)
     """ I had to use charfield instead of genericipaddress
         as my heroku apps were throwing errors """
