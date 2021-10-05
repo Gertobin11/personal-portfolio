addb57a759581731586d40a178c8d315c69e9432
Gertobin11 <gertobin11@gmail.com>
2021-10-05 13:01:08 +0000

    added send_email function too send an email upon a users successful form submission

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render, redirect, reverse
 from .forms import ContactForm1
+from django.conf import settings
+from django.template.loader import render_to_string
 from django.contrib import messages
+from django.core.mail import send_mail
+
+
+def send_confirmation_email(form_data):
+    # Send the user a confirmation email upon completing the contact form
+    form_data = form_data
+    cust_email = form_data['email']
+    subject = render_to_string(
+        'contact/confirmation_emails/confirmation_email_subject.txt',
+        {'form_data': form_data}
+    )
+    body = render_to_string(
+        'contact/confirmation_emails/confirmation_email_body.txt',
+        {'form_data': form_data, 'contact_email': settings.DEFAULT_FROM_EMAIL}
+    )
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [cust_email]
+    )
 
 
 def contact(request):
@@ -16,8 +39,12 @@ def contact(request):
         contact_form = ContactForm1(form_data)
         if contact_form.is_valid():
             contact_form.save()
+            print(form_data)
+
             messages.success(request, 'Thanks for getting in touch, \
                              I will get back to you as fast as I can')
+            # Send the email with the completed form data
+            send_confirmation_email(form_data)
             return redirect(reverse('home'))
     else:
         contact_form1 = ContactForm1
--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -142,3 +142,15 @@ STATICFILES_DIRS = (os.path.join('static'),)
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'the_vinyl_hub@mail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
