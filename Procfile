f4a6d21297aeeea08cbb929c9c092ef225537181
Gertobin11 <gertobin11@gmail.com>
2021-10-07 15:10:33 +0000

    added bid $port to resolve deploy error

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn myportfolio.wsgi:application -preload -b 0.0.0.0:5000 
+web: gunicorn myportfolio.wsgi:application --bind :$PORT
--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ger-tobin-portfolio.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['https://ger-tobin-portfolio.herokuapp.com/', 'localhost']
 
 
 # Application definition
